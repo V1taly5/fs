c1389f2a633e7d6435de97c85381338b6fb70dfb
V1taly5 <egorov21112061vito@gmail.com>
2025-03-09 18:11:59 +0300

    refactor(listener): add max connections limit and improve buffered I/O handling

--- internal/listener/listener.go
+++ internal/listener/listener.go
@@ -9,28 +9,33 @@ import (
 	"fs/internal/util/logger/sl"
 	"log/slog"
 	"net"
+	"sync"
 	"time"
+)
+
+const (
+	defaultPort       = 35035
+	maxConnections    = 100 // Максимальное число одновременных соединений
+	connectionTimeout = 5 * time.Second
 )
 
 func StartListener(ctx context.Context, node *node.Node, port int, log *slog.Logger) {
 	const op = "listener.StartListener"
 	log = log.With(slog.String("op", op))
 
-	// Validate port range
+	// validate port range
 	if port <= 0 || port > 65535 {
-		port = 35035
+		port = defaultPort
 	}
 
 	service := fmt.Sprintf("0.0.0.0:%v", port)
 
-	// Resolve TCP address
 	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	if err != nil {
 		log.Error("Failed to resolve TCP address", sl.Err(err))
 		return
 	}
 
-	// Start listening on the resolved address
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	if err != nil {
 		log.Error("Failed to start TCP listener", slog.String("address", tcpAddr.String()), sl.Err(err))
@@ -38,17 +43,23 @@ func StartListener(ctx context.Context, node *node.Node, port int, log *slog.Log
 	}
 	defer listener.Close()
 
-	log.Info("Service started",
-		slog.String("TCPAddr", tcpAddr.String()),
-	)
+	log.Info("Service started", slog.String("TCPAddr", tcpAddr.String()))
+
+	var wg sync.WaitGroup
+	connLimiter := make(chan struct{}, maxConnections)
 
 	for {
 		select {
 		case <-ctx.Done():
 			log.Info("Shutting down listener")
+			wg.Wait()
 			return
 		default:
-			listener.SetDeadline(time.Now().Add(1 * time.Second))
+			// listener.SetDeadline(time.Now().Add(1 * time.Second))
+			if err := listener.SetDeadline(time.Now().Add(1 * time.Second)); err != nil {
+				log.Warn("Failed to set deadline", sl.Err(err))
+			}
+
 			conn, err := listener.Accept()
 			if err != nil {
 				if ne, ok := err.(net.Error); ok && ne.Timeout() {
@@ -57,48 +68,42 @@ func StartListener(ctx context.Context, node *node.Node, port int, log *slog.Log
 				log.Warn("Error accepting connection", sl.Err(err))
 				continue
 			}
+			select {
+			case connLimiter <- struct{}{}:
+				wg.Add(1)
+				go func() {
+					handleConnection(ctx, conn, node, log)
+					<-connLimiter
+					wg.Done()
+				}()
+			default:
+				log.Warn("Too many connections, rejecting new connection", slog.String("RemoteAddr", conn.RemoteAddr().String()))
+				conn.Close()
+			}
 
-			go func() {
-				handleConnection(ctx, conn, node, log)
-			}()
 		}
 	}
-
-	// for {
-	// 	conn, err := listener.Accept()
-	// 	if err != nil {
-	// 		log.Warn("Error accepting connection", sl.Err(err))
-	// 		continue
-	// 	}
-	// 	go onConection(conn, node, log)
-	// }
 }
 
 func handleConnection(ctx context.Context, conn net.Conn, n *node.Node, log *slog.Logger) {
 	const op = "listener.handleConnection"
-	log = log.With(slog.String("op", op))
+	log = log.With(slog.String("op", op), slog.String("RemoteAddr", conn.RemoteAddr().String()))
 
 	defer func() {
 		conn.Close()
-		log.Info("Connection closed", slog.String("RemoteAddr", conn.RemoteAddr().String()))
+		log.Info("Connection closed")
 	}()
 
-	log.Info("New connection established",
-		slog.String("RemoteAddr", conn.RemoteAddr().String()),
-	)
+	log.Info("New connection established")
 
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
-
 	readWriter := bufio.NewReadWriter(reader, writer)
 
-	//	buf, err := readWriter.Peek(4)
-	//	if err != nil {
-	//		if err != io.EOF {
-	//			log.Default().Printf("Read peak ERROR: %s", err)
-	//		}
-	//		return
-	//	}
 	peer := peers.NewPeer(conn)
 	n.HandleNode(ctx, readWriter, peer)
+
+	if err := writer.Flush(); err != nil {
+		log.Warn("Failed to flush writer", sl.Err(err))
+	}
 }
