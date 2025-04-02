5d99f152a609edb495e91eeaf0aad07d341515f1
V1taly5 <egorov21112061vito@gmail.com>
2025-04-02 20:53:47 +0300

    feat: implement TCPConnection message handling and transmission

--- internal/connection_manager/connection_factory.go
+++ internal/connection_manager/connection_factory.go
@@ -2,13 +2,20 @@ package connectionmanager
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"fs/internal/node"
 	nodestorage "fs/internal/storage/node_storage"
+	"fs/internal/util/logger/sl"
+	fsv1 "fs/proto/gen/go"
+	"io"
 	"log/slog"
 	"net"
+	"os"
 	"time"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func NewConnectionFactory(node *node.Node, log *slog.Logger) *ConnectionFactory {
@@ -110,18 +117,22 @@ func (c *TCPConnector) Name() string {
 	return "tcp"
 }
 
+var bufferSize = 5
+
 // TCP Connection
 type TCPConnection struct {
-	conn     net.Conn
-	endpoint nodestorage.Endpoint
-	active   bool
+	conn        net.Conn
+	endpoint    nodestorage.Endpoint
+	active      bool
+	messageChan chan *fsv1.Message
 }
 
 func NewTCPConnection(conn net.Conn, endpoint nodestorage.Endpoint) *TCPConnection {
 	return &TCPConnection{
-		conn:     conn,
-		endpoint: endpoint,
-		active:   true,
+		conn:        conn,
+		endpoint:    endpoint,
+		active:      true,
+		messageChan: make(chan *fsv1.Message, bufferSize),
 	}
 }
 
@@ -143,9 +154,109 @@ func (c *TCPConnection) Close() error {
 		return nil
 	}
 	c.active = false
+	close(c.messageChan)
 	return c.conn.Close()
 }
 
 func (c *TCPConnection) IsActive() bool {
 	return c.active
+}
+
+func (c *TCPConnection) MessageChannel() <-chan *fsv1.Message {
+	return c.messageChan
+}
+
+func (c *TCPConnection) SendMessage(ctx context.Context, msg *fsv1.Message) error {
+	if !c.active {
+		return errors.New("connection is not active")
+	}
+
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	// добавить размер сообщение как префикс
+	messageSize := uint32(len(data))
+	sizeBuf := make([]byte, 4)
+	binary.BigEndian.PutUint32(sizeBuf, messageSize)
+
+	if deadline, ok := ctx.Deadline(); ok {
+		if err := c.conn.SetWriteDeadline(deadline); err != nil {
+			return fmt.Errorf("failed to set write deadline: %w", err)
+		}
+		defer c.conn.SetWriteDeadline(time.Time{})
+	}
+
+	if _, err := c.conn.Write(sizeBuf); err != nil {
+		c.active = false
+		c.Close()
+		return fmt.Errorf("failed to send message size: %w", err)
+	}
+
+	if _, err := c.conn.Write(data); err != nil {
+		c.Close()
+		return fmt.Errorf("failed to send message size: %w", err)
+	}
+	return nil
+}
+
+// io.ReadFull блокируется и если не поймать закрытие ctx в момент прохода этой проверки,
+//
+//	то функции не закончит свое выполнение, для того, что бы выйти из блокировки io.ReadFull надо закрыть
+//
+// net.Conn -> тогда произойдет разблокировка
+func (c *TCPConnection) StartReading(ctx context.Context) {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	go func() {
+		defer c.Close()
+
+		sizeBuf := make([]byte, 4)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			if !c.IsActive() {
+				return
+			}
+			if _, err := io.ReadFull(c.conn, sizeBuf); err != nil {
+				log.Error("failed to read message size", slog.String("from", c.Address()), sl.Err(err))
+				return
+			}
+
+			messageSize := binary.BigEndian.Uint32(sizeBuf)
+			if messageSize > 10*1024*1024 {
+				log.Error("message size too large", slog.String("from", c.Address()), slog.Uint64("size", uint64(messageSize)))
+				return
+			}
+
+			messageBuff := make([]byte, messageSize)
+			if n, err := io.ReadFull(c.conn, messageBuff); err != nil {
+				localLog := log.With(slog.String("from", c.Address()))
+				if errors.Is(err, io.EOF) {
+					localLog.Info("client has closed connection (EOF)")
+					return
+				} else if errors.Is(err, io.ErrUnexpectedEOF) {
+					localLog.Info("fewer bytes were read than expected client may have closed the connection", slog.Int("bites read", n))
+					return
+				}
+				localLog.Error("failed to read message data", sl.Err(err))
+				return
+			}
+
+			msg := &fsv1.Message{}
+			if err := proto.Unmarshal(messageBuff, msg); err != nil {
+				log.Error("failed to unmarshal message", slog.String("from", c.Address()), sl.Err(err))
+				continue
+			}
+
+			select {
+			case c.messageChan <- msg:
+			case <-time.After(DefaultMessageProcessTimeout):
+				log.Warn("message channel full, dropping message", slog.String("from", c.Address()))
+			}
+		}
+	}()
 }
