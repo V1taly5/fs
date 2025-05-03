20f889cec28f2f4d308ea62a259d6bd465edf737
V1taly5 <egorov21112061vito@gmail.com>
2025-05-03 10:57:21 +0300

    feat: Compare Indexes has been changed to work with proto

--- internal/node/node.go
+++ internal/node/node.go
@@ -15,12 +15,14 @@ import (
 	"fs/internal/peers"
 	"fs/internal/util/logger/sl"
 	"fs/internal/watcher"
+	fsv1 "fs/proto/gen/go"
 	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
 	"reflect"
 	"sync"
+	"time"
 )
 
 const basePath = "/home/vito/Source/Documents"
@@ -75,6 +77,7 @@ func NewNode(name string, port int, log *slog.Logger) *Node {
 	indexerConfig := indexer.IndexerConfig{
 		Database:     db,
 		BlockStorage: blockStorage,
+		RootDir:      "/home/vito/Source/projects/fs/testFolder",
 		Hasher:       hasher,
 		Logger:       nil,
 	}
@@ -346,6 +349,45 @@ func compareBlocks(localFile *indexer.FileIndex, remoteFile *indexer.FileIndex) 
 	return missingBlocks
 }
 
+// compareBlocks compares local and remote blocks to find what's missing locally
+func compareBlocksNew(localFile, remoteFile *indexer.FileIndex) []int {
+	localBlockMap := make(map[int][32]byte)
+	for _, block := range localFile.Blocks {
+		localBlockMap[block.Index] = block.Hash
+	}
+
+	var missingIndices []int
+	for _, remoteBlock := range remoteFile.Blocks {
+		localHash, exists := localBlockMap[remoteBlock.Index]
+		if !exists || localHash != remoteBlock.Hash {
+			missingIndices = append(missingIndices, remoteBlock.Index)
+		}
+	}
+
+	return missingIndices
+}
+
+func compareBlocksPtr(localFile *indexer.FileIndex, remoteFile *indexer.FileIndex) []*BlockRequest {
+	localBlocks := make(map[int][32]byte)
+	for _, block := range localFile.Blocks {
+		localBlocks[block.Index] = block.Hash
+	}
+
+	var missingBlocks []*BlockRequest
+	for _, remoteBlock := range remoteFile.Blocks {
+		localHash, exists := localBlocks[remoteBlock.Index]
+		if !exists || localHash != remoteBlock.Hash {
+			// Блок отсутствует локально или отличается
+			br := &BlockRequest{
+				FilePath:   remoteFile.Path,
+				BlockIndex: remoteBlock.Index,
+			}
+			missingBlocks = append(missingBlocks, br)
+		}
+	}
+	return missingBlocks
+}
+
 func (n *Node) readBlock(filePath string, blockIndex int) ([]byte, error) {
 	const BlockSize = 128 * 1024 // Размер блока: 128 КБ
 
@@ -465,4 +507,212 @@ func (n *Node) HandleNode(ctx context.Context, rw *bufio.ReadWriter, peer *peers
 			handler(peer, cover)
 		}
 	}
+}
+
+func (n *Node) CompareIndexes(remoteFiles []*indexer.FileIndex) []*fsv1.BlockRequest {
+	const op = "node.compareIndexes"
+	log := n.log.With(slog.String("op", op))
+	log.Debug("inside node.compareIndexes")
+
+	localFiles, err := n.IndexDB.GetAllFileIndexes()
+	if err != nil {
+		log.Error("Failed to get local file indexes: ", sl.Err(err))
+		return nil
+	}
+
+	localIndexMap := make(map[string]*indexer.FileIndex)
+	for _, fi := range localFiles {
+		localIndexMap[fi.Path] = fi
+	}
+
+	remoteIndexMap := make(map[string]*indexer.FileIndex)
+	for _, fi := range remoteFiles {
+		remoteIndexMap[fi.Path] = fi
+	}
+
+	var blockRequests []*fsv1.BlockRequest
+
+	for path, remoteFile := range remoteIndexMap {
+		localFile, exists := localIndexMap[path]
+
+		if !exists {
+			// file doesn't exist locally, request all blocks
+			log.Debug("file doesn't exist locally, requesting all blocks",
+				slog.String("path", path),
+			)
+			blockRequests = append(blockRequests, createBlockRequestsForFile(path, remoteFile.Blocks)...)
+		} else if localFile.FileHash != remoteFile.FileHash {
+			// files are different, check for conflicts
+			log.Debug("file hash mismatch",
+				slog.String("path", path),
+				slog.String("local", fmt.Sprintf("%x", localFile.FileHash)),
+				slog.String("remote", fmt.Sprintf("%x", remoteFile.FileHash)),
+			)
+
+			if n.shouldHandleAsConflict(localFile, remoteFile) {
+				// handle conflict case
+				conflictRequest := n.handleConflictNew(localFile, remoteFile)
+				if len(conflictRequest) > 0 {
+					blockRequests = append(blockRequests, conflictRequest...)
+				}
+			} else {
+				// no conflict, just get missing blocks
+				missingBlocks := compareBlocksNew(localFile, remoteFile)
+				if len(missingBlocks) > 0 {
+					log.Debug("requesting missing blocks",
+						slog.String("path", path),
+						slog.Int("count", len(missingBlocks)),
+					)
+					fsv1Missing := convertToFsv1BlockRequests(path, missingBlocks)
+					blockRequests = append(blockRequests, fsv1Missing...)
+				}
+			}
+		} else {
+			log.Debug("file is up to date", slog.String("path", path))
+		}
+	}
+
+	// Check for locally deleted files that still exist remotely
+	for path, localFile := range localIndexMap {
+		if localFile.Deleted {
+			if remoteFile, exists := remoteIndexMap[path]; exists && !remoteFile.Deleted {
+				// handle deleted file conflict
+				log.Debug("conflict: file deleted locally but exists remotely", slog.String("path", path))
+			}
+		}
+	}
+
+	if len(blockRequests) > 0 {
+		log.Info("found blocks to request", slog.Int("count", len(blockRequests)))
+		return blockRequests
+	}
+
+	log.Debug("no blocks to request")
+	return nil
+}
+
+// createBlockRequestsForFile creates block requests for all blocks in a file
+func createBlockRequestsForFile(path string, blocks []indexer.BlockHash) []*fsv1.BlockRequest {
+	// create a request for each block
+	var result []*fsv1.BlockRequest
+
+	for _, block := range blocks {
+		request := &fsv1.BlockRequest{
+			FileId:       path,
+			BlockIndexes: uint32(block.Index),
+		}
+		result = append(result, request)
+	}
+
+	return result
+}
+
+// shouldHandleAsConflict determines if we should handle this as a conflict case
+// A conflict occurs when both local and remote have diverged from their common ancestor
+func (n *Node) shouldHandleAsConflict(localFile, remoteFile *indexer.FileIndex) bool {
+	// If we have no versions, we can't detect conflicts
+	if len(localFile.Versions) == 0 || len(remoteFile.Versions) == 0 {
+		return false
+	}
+
+	// If local file has been modified since last sync
+	if localFile.LastSyncedVersionID != "" &&
+		localFile.LastSyncedVersionID != localFile.Versions[len(localFile.Versions)-1].VersionID {
+
+		// Find the common ancestor version
+		var commonAncestorFound bool
+		for _, localVersion := range localFile.Versions {
+			if localVersion.VersionID == localFile.LastSyncedVersionID {
+				commonAncestorFound = true
+				break
+			}
+		}
+
+		// If we found the common ancestor and remote has also diverged
+		if commonAncestorFound &&
+			remoteFile.LastSyncedVersionID != remoteFile.Versions[len(remoteFile.Versions)-1].VersionID {
+			return true
+		}
+	}
+
+	return false
+}
+
+// handleConflict handles file conflicts by implementing a conflict resolution strategy
+// Returns any block requests needed after conflict resolution
+func (n *Node) handleConflictNew(localFile, remoteFile *indexer.FileIndex) []*fsv1.BlockRequest {
+	const op = "node.handleConflict"
+	log := n.log.With(slog.String("op", op), slog.String("path", localFile.Path))
+	log.Info("handling file conflict")
+
+	// if remote file is newer, get its blocks
+	if remoteFile.ModTime.After(localFile.ModTime) {
+		log.Info("resolving conflict: remote file is newer",
+			slog.Time("remote", remoteFile.ModTime),
+			slog.Time("local", localFile.ModTime),
+		)
+
+		// create conflict file path with timestamp
+		conflictPath := localFile.Path + ".conflict_" + time.Now().Format("20060102_150405")
+
+		// rename local file to conflict file
+		err := os.Rename(localFile.Path, conflictPath)
+		if err != nil {
+			log.Error("failed to create conflict file", sl.Err(err))
+			// continue anyway to sync the remote version
+		} else {
+			log.Info("created conflict file", slog.String("conflict_path", conflictPath))
+		}
+
+		// request all blocks from the remote file
+		return createBlockRequestsForFile(localFile.Path, remoteFile.Blocks)
+	} else {
+		log.Info("resolving conflict: keeping local file (newer)",
+			slog.Time("local", localFile.ModTime),
+			slog.Time("remote", remoteFile.ModTime),
+		)
+
+		return nil
+	}
+}
+
+func ConvertToFsv1BlockRequests(requests []BlockRequest) ([]*fsv1.BlockRequest, error) {
+	if len(requests) == 0 {
+		return nil, fmt.Errorf("пустой список запросов")
+	}
+
+	// группируем BlockIndex по FilePath
+	grouped := make(map[string][]int)
+	for _, req := range requests {
+		grouped[req.FilePath] = append(grouped[req.FilePath], req.BlockIndex)
+	}
+
+	// Создаём срез fsv1.BlockRequest
+	var fsv1Requests []*fsv1.BlockRequest
+	for filePath, indexes := range grouped {
+		for _, index := range indexes {
+			fsv1Req := &fsv1.BlockRequest{
+				FileId:       filePath,
+				BlockIndexes: uint32(index),
+			}
+			fsv1Requests = append(fsv1Requests, fsv1Req)
+		}
+	}
+
+	return fsv1Requests, nil
+}
+
+// convertToFsv1BlockRequests converts a list of block indices to fsv1.BlockRequest objects
+func convertToFsv1BlockRequests(path string, blockIndices []int) []*fsv1.BlockRequest {
+	var result []*fsv1.BlockRequest
+
+	for _, index := range blockIndices {
+		request := &fsv1.BlockRequest{
+			FileId:       path,
+			BlockIndexes: uint32(index),
+		}
+		result = append(result, request)
+	}
+
+	return result
 }
